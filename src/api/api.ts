cec451bab3f9550d8386b8a86604b439a2f536c7
Bryan Aguilar <baguilar@technisys.com>
2023-03-03 16:01:53 -0500

    feat: Delete github api key

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,6 +3,6 @@ import axios from 'axios';
 export const api = axios.create({
   baseURL: `https://api.github.com/repos/facebook/react`,
   headers: {
-    Authorization: `Bearer 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`
+    
   },
 });
